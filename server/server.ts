4066d4d8f31419e38fdfb6256b4fb99176339539
João Neto <joao.neto.ninja@hotmail.com>
2019-01-28 07:15:34 -0300

    habilitando https

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import * as restify from 'restify'
 import * as mongoose from 'mongoose'
 
+import * as fs from 'fs'
+
 import {environment} from './../common/environment'
 import {Router} from './../common/router'
 import {mergePatchBodyParser} from './merge-patch.parser'
@@ -22,7 +24,9 @@ export class Server {
             try {
                 this.application = restify.createServer({
                     name: 'meat-api',
-                    version: '1.0.0'
+                    version: '1.0.0',
+                    certificate: fs.readFileSync('./security/keys/cert.pem'),
+                    key: fs.readFileSync('./security/keys/key.pem')
                 })
                 
                 this.application.use(restify.plugins.queryParser())
